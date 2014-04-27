8b4fde6090b887302ada700f1ac8d478aae16f7b
josisusan <josisusan@gmail.com>
2014-04-27 16:52:33 +0545

    Fix typo.

--- spec/mongoid/document_locker_spec.rb
+++ spec/mongoid/document_locker_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Mongoid::DocumentLocker do
   let(:demo) { Demo.new(name: "foo") }
 
-  it 'should set the lock of document to true'  do
+  it 'should set the lock of document to false'  do
     demo.lock.should be_false
   end
 
